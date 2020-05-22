244f953ac4ef017e1b84b95cef8aa03082165cad
Yvana Wagter <yvana@wagter.nl>
2020-05-22 22:22:52 +0200

    tweaked it a bit

--- valid parentheses.js
+++ valid parentheses.js
@@ -1,22 +1,24 @@
 // Write a function called that takes a string of parentheses, and determines if the order of the parentheses is valid. The function should return true if the string is valid, and false if it 's invalid.
 // Constraints: 0 <= input.length <= 100
 
-function validParentheses(parens){
-  if (parens.length <= 0 || parens.length > 100) return false
-  let array = []
-  for (let i = 0; i < parens.length; i++) {
-    if (parens[i] === '(') {
-            array.push(parens[i])
-        }
-     else if (parens[i] === ')') {
-       if (array.length === 0){
-       array.push(parens[i])
-       }
-       else {
-         let last = array.pop();
+function validParentheses(parens) {
+    if (parens.length < 0 || parens.length > 100) return false
+    let arrayOpen = []
+    let arrayClose = [];
+    for (let i = 0; i < parens.length; i++) {
+        if (parens[i] === '(') {
+            arrayOpen.push(parens[i])
+        } else if (parens[i] === ')') {
+            if (arrayOpen.length === 0) {
+                arrayClose.push(parens[i])
+            } else {
+                let last = arrayOpen.pop();
+            }
         }
     }
-  }
-   if (array.length === 0) {return true}
-   else {return false}
+    if (arrayOpen.length === 0 && arrayClose.length === 0) {
+        return true
+    } else {
+        return false
+    }
 }
